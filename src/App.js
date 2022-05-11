7be770f9767ac9d0a48ec8636bc8cd99058dfbac
Jair montenegro <jairmontenegrof@gmail.com>
2022-05-11 19:45:29 -0300

    add previus project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import './App.css';
-
+import Footer from "./components/footer.js"
 
 
 function App() {
   return (
     <div className="App">
-      react
-    </div>
+
+      <div className='wrapper'>
+      <header>To-Do List</header>
+      <div className="inputField">
+            <input type="text" placeholder="Add Your new todo"/>
+            <button><i className="fas fa-plus"></i></button>
+        </div>
+        <ul className="todoList">
+            {/* <!-- addd --> */}
+        </ul>
+
+        <div className="footer">
+            <span>You have <span className="pendingNumb"></span> pending tasks </span>
+            <button>Clear All</button>
+        </div>
+        </div>
+      <Footer/>
+      </div>
   );
 }
 
