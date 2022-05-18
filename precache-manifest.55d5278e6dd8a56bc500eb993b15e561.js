ed55947f43ce8b45ec2938b43693874198014eb4
Jair montenegro <jairmontenegrof@gmail.com>
2022-05-18 12:33:28 -0300

    Updates

---
+++ precache-manifest.55d5278e6dd8a56bc500eb993b15e561.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0920ad1fc2747341f8c0",
+    "url": "/To-Do-App-React-r/static/css/main.9b069677.chunk.css"
+  },
+  {
+    "revision": "0920ad1fc2747341f8c0",
+    "url": "/To-Do-App-React-r/static/js/main.3d67c6a9.chunk.js"
+  },
+  {
+    "revision": "eefcf4cc3113abcdcec1",
+    "url": "/To-Do-App-React-r/static/js/runtime~main.2cbb2f80.js"
+  },
+  {
+    "revision": "d2d33b13dfd15e87a82b",
+    "url": "/To-Do-App-React-r/static/js/2.c521f056.chunk.js"
+  },
+  {
+    "revision": "2beaf3eb2ce04aa685c3ddcf7779a7c9",
+    "url": "/To-Do-App-React-r/index.html"
+  }
+];
